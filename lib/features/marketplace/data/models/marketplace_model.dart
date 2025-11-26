50f348d8f9158dc28dc44b4d6cf27aa9b79edc07
amberleyvs <amberley.vidya41@ui.ac.id>
2025-11-26 13:36:14 +0700

    feat: add marketplace model

--- lib/features/marketplace/data/models/marketplace_model.dart
+++ lib/features/marketplace/data/models/marketplace_model.dart
@@ -1,5 +1,99 @@
-﻿class MarketplaceModel {
-  // TODO: define fields
+﻿// To parse this JSON data, do
+//
+//     final marketplaceModel = marketplaceModelFromJson(jsonString);
 
-  MarketplaceModel();
+import 'dart:convert';
+
+List<MarketplaceModel> marketplaceModelFromJson(String str) => List<MarketplaceModel>.from(json.decode(str).map((x) => MarketplaceModel.fromJson(x)));
+
+String marketplaceModelToJson(List<MarketplaceModel> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+class MarketplaceModel {
+    Model model;
+    String pk;
+    Fields fields;
+
+    MarketplaceModel({
+        required this.model,
+        required this.pk,
+        required this.fields,
+    });
+
+    factory MarketplaceModel.fromJson(Map<String, dynamic> json) => MarketplaceModel(
+        model: modelValues.map[json["model"]]!,
+        pk: json["pk"],
+        fields: Fields.fromJson(json["fields"]),
+    );
+
+    Map<String, dynamic> toJson() => {
+        "model": modelValues.reverse[model],
+        "pk": pk,
+        "fields": fields.toJson(),
+    };
+}
+
+class Fields {
+    int owner;
+    String title;
+    int price;
+    Condition condition;
+    String location;
+    String imageUrl;
+
+    Fields({
+        required this.owner,
+        required this.title,
+        required this.price,
+        required this.condition,
+        required this.location,
+        required this.imageUrl,
+    });
+
+    factory Fields.fromJson(Map<String, dynamic> json) => Fields(
+        owner: json["owner"],
+        title: json["title"],
+        price: json["price"],
+        condition: conditionValues.map[json["condition"]]!,
+        location: json["location"],
+        imageUrl: json["image_url"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "owner": owner,
+        "title": title,
+        "price": price,
+        "condition": conditionValues.reverse[condition],
+        "location": location,
+        "image_url": imageUrl,
+    };
+}
+
+enum Condition {
+    BRAND_NEW,
+    USED
+}
+
+final conditionValues = EnumValues({
+    "BRAND_NEW": Condition.BRAND_NEW,
+    "USED": Condition.USED
+});
+
+enum Model {
+    MARKETPLACE_MODULE_LISTING
+}
+
+final modelValues = EnumValues({
+    "marketplace_module.listing": Model.MARKETPLACE_MODULE_LISTING
+});
+
+class EnumValues<T> {
+    Map<String, T> map;
+    late Map<T, String> reverseMap;
+
+    EnumValues(this.map);
+
+    Map<T, String> get reverse {
+            reverseMap = map.map((k, v) => MapEntry(v, k));
+            return reverseMap;
+    }
 }
